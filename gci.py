bd5bd167738ea476c5ca290d7b7f356699edd6e6
vibham9876 <44752321+vibham9876@users.noreply.github.com>
2018-11-04 23:45:35 +0530

    qwerty
    this code reverses a string

---
+++ gci.py
@@ -0,0 +1,9 @@
+for i in range(11):
+    print ("GCI is great")
+
+
+
+a=str(input("Enter your name:-"))
+b=(a[::-1])
+print ("Hello",a,"please to meet you!Did you know that your name backward is,",b)
+      
